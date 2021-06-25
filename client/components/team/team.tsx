2b4910730904c13c391d7488a817008cde890f16
saxenabhishek <as7122000@gmail.com>
2021-06-25 15:42:19 +0530

    ✨ team page icons

--- client/components/team/team.tsx
+++ client/components/team/team.tsx
@@ -6,23 +6,27 @@ const Team = () => {
     {
       name: "Abhishek Saxena",
       gitHub_id: "URL",
-      handles: [{ linkedin: "aasa" }],
+      handles: [
+        { linkedin: "aasa" },
+        { github: "fakegithub" },
+        { portfolio: "something" },
+      ],
       img_url: "./test.webp",
       tagline: "line1",
     },
     {
       name: " sakshi",
       gitHub_id: "URL",
-      handles: [{ linkedin: "aasa" }],
+      handles: [{ linkedin: "aasa" }, { portfolio: "something" }],
       img_url: "./test2.webp",
       tagline: "line2",
     },
   ];
 
   return (
-    <div>
+    <div className="flex flex-wrap">
       {members.map((member) => (
-        <Member key={user.gitHub_id} member={member} />
+        <Member key={member.gitHub_id} member={member} />
       ))}
     </div>
   );
