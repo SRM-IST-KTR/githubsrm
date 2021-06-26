a9fefdf80f6f009e1211b094ee6c8cc73ff44812
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-06-26 21:39:55 +0530

    🚀 switched to production

--- server/githubsrm/core/settings.py
+++ server/githubsrm/core/settings.py
@@ -24,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
