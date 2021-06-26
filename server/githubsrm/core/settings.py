f1745473924e5eae96c6b3a260610d579a46bdd4
Riju Mukherjee <rmukh561@gmail.com>
2021-06-26 23:44:15 +0530

    🐛 fix settings.py

--- server/githubsrm/core/settings.py
+++ server/githubsrm/core/settings.py
@@ -56,6 +56,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost:8000"
+]
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'core.urls'
