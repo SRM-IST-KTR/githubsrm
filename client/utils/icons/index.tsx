5572554d6ea8095e0e4b12cbee7b7f9cc509822a
jagnani73 <yashjagnani73@gmail.com>
2021-06-27 00:08:46 +0530

    💫 add icon
    Co-authored-by: Sakshi <choudharysakshi023@gmail.com>
    Co-authored-by: saxenabhishek <as7122000@gmail.com>

--- client/utils/icons/index.tsx
+++ client/utils/icons/index.tsx
@@ -5,3 +5,4 @@ export { default as TwitterIcon } from "./twitter";
 export { default as BookIcon } from "./book";
 export { default as PersonIcon } from "./person";
 export { default as BranchIcon } from "./branch";
+export { default as ArrowIcon } from "./arrow";
