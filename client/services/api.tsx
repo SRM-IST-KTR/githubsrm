92d3583d383ce50f0d5210dc318681ac357052ed
jagnani73 <yashjagnani73@gmail.com>
2021-06-27 00:09:51 +0530

    👽️ add service
    Co-authored-by: Sakshi <choudharysakshi023@gmail.com>
    Co-authored-by: saxenabhishek <as7122000@gmail.com>

---
+++ client/services/api.tsx
@@ -0,0 +1,95 @@
+import axios, { AxiosInstance, AxiosError } from "axios";
+
+import {
+  MemberProps,
+  ContributorFormData,
+  MaintainerFormData,
+} from "../utils/interfaces";
+import { getRecaptchaToken } from "./recaptcha";
+
+const instance: AxiosInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`,
+});
+
+export const getTeam = async (): Promise<MemberProps[] | false> => {
+  try {
+    const team = await (await instance.get("/team")).data;
+    return team;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const postContributor = async (
+  data: ContributorFormData
+): Promise<boolean> => {
+  try {
+    const recaptchaToken = await getRecaptchaToken("post");
+    const res = await instance.post("/contributor", data, {
+      headers: {
+        X_RECAPTCHA_TOKEN: recaptchaToken,
+      },
+    });
+    console.log(res);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const postMaintainer = async (
+  data: MaintainerFormData
+): Promise<boolean> => {
+  console.log(data);
+  return true;
+};
+
+// const errorHandler = (err?: AxiosError | any) => {
+//   let errMessage: string = "Oops! Something went wrong.";
+//   if (err) {
+//     switch (err.response?.status) {
+//       case 400:
+//         errMessage = "Bad request. Kindly check your inputs.";
+//         break;
+//       case 401:
+//         errMessage = "Unauthorized.";
+//         break;
+//       case 403:
+//         errMessage = "Forbidden.";
+//         break;
+//       case 409:
+//         errMessage = "Already exists.";
+//         break;
+//       case 500:
+//         errMessage = "Internal server error.";
+//         break;
+//       default:
+//         errMessage = "Oops! Something went wrong.";
+//         break;
+//     }
+//   }
+
+//   toast.error(errMessage, {
+//     position: "bottom-left",
+//     autoClose: 5000,
+//     hideProgressBar: false,
+//     closeOnClick: true,
+//     pauseOnHover: true,
+//     draggable: true,
+//     progress: undefined,
+//   });
+// };
+
+// const successHandler = (errMessage: string) => {
+//   toast.success(errMessage, {
+//     position: "bottom-left",
+//     autoClose: 5000,
+//     hideProgressBar: false,
+//     closeOnClick: true,
+//     pauseOnHover: true,
+//     draggable: true,
+//     progress: undefined,
+//   });
+// };
