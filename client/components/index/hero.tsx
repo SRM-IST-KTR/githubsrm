c9771519ff3127a23baa0ad12766e213bc20168b
jagnani73 <yashjagnani73@gmail.com>
2021-06-27 00:10:17 +0530

    🍻 404
    Co-authored-by: Sakshi <choudharysakshi023@gmail.com>
    Co-authored-by: saxenabhishek <as7122000@gmail.com>

--- client/components/index/hero.tsx
+++ client/components/index/hero.tsx
@@ -5,36 +5,43 @@ interface HeroProps {
 }
 
 const Hero = ({ reference }: HeroProps) => {
-  const domainData: Array<{ img: string; alt: string; text: string }> = [
+  const domainData: Array<{
+    img: string;
+    alt: string;
+    text: string;
+  }> = [
     {
       img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
       alt: "logo",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ever since the 1500s.",
+      text: "Students can contribute to Open Source Projects under the banner of SRMIST allowing students to get industry-level exposure through SRMIST’s organization.",
     },
     {
       img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
       alt: "logo",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ever since the 1500s.",
+
+      text: "It also helps us maintain a record of all the contributions students have made under the banner of SRMIST.      ",
     },
     {
       img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
       alt: "logo",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ever since the 1500s.",
+
+      text: "Travis CI Enterprise license. Travis CI is a continuous integration platform, allowing to run CI/CD pipelines where builds, unit tests, and integration tests can be executed.",
     },
     {
       img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
       alt: "logo",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ever since the 1500s.",
+      text: "A GitHub Education Swag Bag for your students every semester.",
+    },
+
+    {
+      img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
+      alt: "logo",
+      text: "Inculcates healthy competition within students encouraging them to contribute further to industry-level projects present in the open-source domain under the banner of SRMIST.",
     },
     {
       img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
       alt: "logo",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ever since the 1500s.",
-    },
-    {
-      img: "https://img.stackshare.io/service/10608/default_2e4e6445d2b6326eb7c7748f17ae5109a99121fc.png",
-      alt: "logo",
-      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ever since the 1500s.",
+      text: "This initiative can lead to major project development/contributions to large organizations under the banner of SRMIST.",
     },
   ];
 
@@ -50,24 +57,28 @@ const Hero = ({ reference }: HeroProps) => {
         GitHub SRM
       </motion.h1>
 
-      <div>
-        <h2 className="text-4xl font-semibold text-base-black py-4 my-4 text-center">
-          OUR FOCUS
-        </h2>
-      </div>
-
       <div className="flex flex-col-reverse  lg:flex-row justify-center items-center">
-        <p className="text-xl lg:w-8/12 mx-auto py-2 my-2 text-justify items-center">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        <div className="text-xl lg:w-8/12 mx-auto py-2 my-2 text-justify items-center">
+          <p>
+            GitHub Community SRM is the official/foremost student-led community
+            spearheading the Open Source Revolution at SRMIST, Chennai.
+          </p>
+          <br />
+          <p>
+            We have successfully brought the GitHub Campus Partner Program to
+            SRMIST, enabling all SRMIST students to apply for the Github
+            Externship Program. In addition, the program has given SRMIST access
+            to Premium Enterprise licensing to GitHub, Travis, and GitKraken
+            accounts.
+          </p>
+          <br />
+          <p>
+            We plan on administering the SRMIST KTR GitHub Enterprise
+            organization, helping the faculties in managing student projects,
+            and helping the students get onboarded and get started with their
+            projects.
+          </p>
+        </div>
 
         <figure className="w-3/12">
           <img
@@ -80,7 +91,7 @@ const Hero = ({ reference }: HeroProps) => {
 
       <div className="flex flex-col justify-center">
         <p className="text-4xl font-semibold text-base-black py-4 my-4 text-center">
-          OUR DOMAINS
+          Benefits of being a GitHub Campus Partner Program
         </p>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-1">
@@ -90,26 +101,29 @@ const Hero = ({ reference }: HeroProps) => {
                 <figure className="mx-auto rounded-full overflow-hidden">
                   <img src={data.img} alt={data.alt} />
                 </figure>
-                <p className="mt-8">{data.text}</p>
+
+                <p className="text-md mt-5">{data.text}</p>
               </div>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="flex flex-col justify-items-center my-20  items-center">
-        <div className="flex my-10  justify-items-center ">
-          <div>
-            <div className="px-32 py-48 bg-gray-100 mb-5 shadow-xl rounded-xl">
-              abc
+      <div className="hidden lg:block">
+        <div className="flex flex-col justify-items-center my-20  items-center">
+          <div className="flex my-10  justify-items-center ">
+            <div>
+              <div className="px-32 py-48 bg-gray-100 mb-5 shadow-xl rounded-xl">
+                abc
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col ml-5 ">
-            <div className="py-32 px-44 mb-5 bg-gray-100 shadow-xl rounded-xl">
-              abc
-            </div>
-            <div className="w-3/4 py-20 text-center bg-gray-100  shadow-xl rounded-xl">
-              abc
+            <div className="flex flex-col ml-5 ">
+              <div className="py-32 px-44 mb-5 bg-gray-100 shadow-xl rounded-xl">
+                abc
+              </div>
+              <div className="w-3/4 py-20 text-center bg-gray-100  shadow-xl rounded-xl">
+                abc
+              </div>
             </div>
           </div>
         </div>
--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,5 +1,35 @@
+import { useRef } from "react";
+import { useRouter } from "next/router";
+import { motion, useElementScroll, useTransform } from "framer-motion";
+
+import { Navbar } from "../components/shared";
+import { OSS, Team, JoinUs, Projects } from "../utils/titles";
+
 const NotFound = () => {
-  return <div>404</div>;
+  const { pathname } = useRouter();
+  const scrollDivRef = useRef<HTMLDivElement>();
+  const { scrollYProgress } = useElementScroll(scrollDivRef);
+  const opacity = useTransform(scrollYProgress, [0, 0.15], [1, 0]);
+  const Y = useTransform(scrollYProgress, [0, 0.2], [0, 100]);
+
+  return (
+    <>
+      <div className="fixed w-full h-screen bg-gradient-to-b from-base-blue to-base-black -z-10" />
+      <div className="flex flex-col w-11/12 h-screen pt-16 mx-auto">
+        <Navbar />
+
+        {/* scrollable */}
+        <div
+          ref={scrollDivRef}
+          className="w-full relative mx-auto h-full bg-base-black overflow-scroll no-scrollbar text-9xl font-extrabold text-base-smoke text-center "
+        >
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
