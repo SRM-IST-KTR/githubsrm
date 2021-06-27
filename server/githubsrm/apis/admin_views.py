6ead23bf3b209aa9e9765bffdc846d5a71e045a2
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-06-27 22:22:49 +0530

    🔥 changed schema

--- server/githubsrm/apis/admin_views.py
+++ server/githubsrm/apis/admin_views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from .perms import CustomPerms
 from .models import Entry
 from rest_framework import response, status
-from .definitions import AdminSchema
+
 
 entry = Entry()
 
@@ -25,26 +25,8 @@ class AdminContributor(APIView):
         Returns:
             response.Response
         """
-        valid = AdminSchema(data=request.data, headers={
-            "path_info": request.path_info
-        }, method='PUT').valid()
-
-        if 'error' not in valid:
-            result = entry.approve_contributor(identifier=valid.get(
-                'contributor_id'), project_id=valid.get('project_id'))
-            if result:
-                return response.Response(data={
-                    "approved": f"{request.data.get('contributor_id')}"
-                }, status=status.HTTP_200_OK)
-            return response.Response(data={
-                "invalid id": request.data
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        return response.Response({
-            "invalid data": request.data,
-            "error": valid.get('error')
-        })
-
+        pass
+        
 
 class AdminMaintainer(APIView):
     """Admin routes for maintainer
@@ -53,3 +35,4 @@ class AdminMaintainer(APIView):
         APIView 
     """
     permission_classes = [CustomPerms]
+    pass
