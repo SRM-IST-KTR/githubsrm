42378e9d7f0f8de59734a3dfb96b47221d529ec5
saxenabhishek <as7122000@gmail.com>
2021-06-27 22:04:44 +0530

    💄  Join us

--- client/components/join-us/join-us.tsx
+++ client/components/join-us/join-us.tsx
@@ -1,63 +1,66 @@
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
 import Link from "next/link";
-import { motion } from "framer-motion";
 import { ArrowIcon } from "../../utils/icons";
 
 const test = () => {
-  let [formOption, setFormOption] = useState(false);
+  let [formOption, setFormOption] = useState(true);
 
-  // //TODO: Put inside one object
-  const list1 = ["Contributor", "Maintainer"];
   const href = ["/join-us/contributor", "/join-us/maintainer"];
   return (
     <>
-      <h1 className="font-medium text-5xl">Join us as a?</h1>
-      <section className="container flex flex-row items-stretch">
+      <h1 className="font-medium ml-16 text-5xl">Join us as a?</h1>
+      <section className="flex flex-row h-full">
         <div className="w-1/3 p-4">
-          <div
-            className={`flex flex-col ${
-              formOption ? "justify-start" : "justify-end"
-            } items-center`}
-          >
+          <div className={`flex flex-col justify-evenly items-center`}>
             <span
               onClick={() => setFormOption(true)}
               className={` ${
-                !formOption ? "text-xl" : "text-4xl text-base-green"
-              } h-36 flex flex-row items-center cursor-pointer`}
+                !formOption ? "text-xl" : "text-2xl text-base-green"
+              } h-48 flex flex-row items-center cursor-pointer`}
             >
               Contributor
-              <div className="w-5 h-5">
+              {/* <div className="w-5 h-5">
                 <ArrowIcon />
-              </div>
+              </div> */}
             </span>
             <span
               onClick={() => setFormOption(false)}
               className={` ${
-                formOption ? "text-xl" : "text-4xl text-base-green"
-              } h-36 flex flex-row items-center cursor-pointer`}
+                formOption ? "text-xl" : "text-2xl text-base-green"
+              } h-48 flex flex-row items-center cursor-pointer`}
             >
               Maintainer
-              <div className="w-5 h-5">
+              {/* <div className="w-5 h-5">
                 <ArrowIcon />
-              </div>
+              </div> */}
             </span>
           </div>
         </div>
-        <div className=" w-full h-auto p-12 ">
-          <div className=" p-4 bg-base-smoke border-t-4 rounded-sm border-base-green shadow-lg h-full">
+        <div className=" w-full p-12 ">
+          <div className=" p-8 bg-base-smoke border-t-4 rounded-sm border-base-green shadow-lg">
             {formOption ? (
               <>
-                <h2 className="text-lg">Your Job as a Contributor</h2>
-                <p>Stuff You would do</p>
+                <h2 className="text-4xl mb-4">Your Job as a Contributor</h2>
+                <p className="text-md">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                  dolores deserunt ea doloremque natus error, rerum quas quaerat
+                  nam ex commodi hic, suscipit in a veritatis pariatur minus
+                  consequuntur!
+                </p>
               </>
             ) : (
               <>
-                <h2 className="text-lg">Your Job as a Maintainer</h2>
-                <p>Stuff You would do</p>
+                <h2 className="text-4xl mb-4">Your Job as a Maintainer</h2>
+                <p className="text-md">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                  dolores deserunt ea doloremque natus error, rerum quas quaerat
+                  nam ex commodi hic, suscipit in a veritatis pariatur minus
+                  consequuntur!o
+                </p>
               </>
             )}
           </div>
-          <div className=" w-32 text-center text-white mt-2 p-4 rounded-sm bg-base-teal ml-auto mr-3">
+          <div className=" w-32 text-center text-white my-8 p-4 rounded-md hover:bg-base-blue bg-base-teal ml-auto mr-3">
             {formOption ? (
               <Link href={href[0]}>Contributor</Link>
             ) : (
--- client/components/team/team.tsx
+++ client/components/team/team.tsx
@@ -13,9 +13,8 @@ const Team = ({ team }: TeamProps) => {
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 text-center">
-        {team.map((member) => (
-          <Member key={member.id} member={member} />
-        ))}
+        {team &&
+          team.map((member) => <Member key={member.id} member={member} />)}
       </div>
     </div>
   );
