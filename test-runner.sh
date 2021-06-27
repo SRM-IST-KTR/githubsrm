03890dd3620d22dcf6b3345c2f32e6571874b1db
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-06-27 12:12:30 +0530

    🚧 CI

--- test-runner.sh
+++ test-runner.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "SECRET_KEY=$SECRET_KEY" >> .env
-echo "TEST_MONGO_DB=$TEST_MONGO_DB" >> .env
-echo "MONGO_DB=$MONGO_DB" >> .env
-echo "MONGO_URI=$MONGO_URI" >> .env
 parallel -j 2 :::: test-config.txt
 exit 0
