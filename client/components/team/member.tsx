6262365e9b433f294ef2c8c83e7fcbbd48d057c5
saxenabhishek <as7122000@gmail.com>
2021-06-29 01:48:49 +0530

    💄 responsive team page

--- client/components/team/member.tsx
+++ client/components/team/member.tsx
@@ -1,6 +1,5 @@
 import Socials from "./socials";
 import { motion } from "framer-motion";
-
 import { MemberProps } from "./../../utils/interfaces";
 
 interface MemberProp {
@@ -9,12 +8,26 @@ interface MemberProp {
 
 const Profile = ({ member }: MemberProp) => {
   const holder = {
-    initial: { scale: 1 },
+    initial: () => {
+      if (window.innerWidth < 786) {
+        return { scale: 1.1 };
+      } else {
+        return { scale: 1 };
+      }
+    },
     hover: { scale: 1.1 },
   };
   const img = {
-    initial: { scale: 1 },
-    hover: { scale: 0.8 },
+    initial: () => {
+      if (window.innerWidth > 786) {
+        return { scale: 1 };
+      } else {
+        return { scale: 0.8 };
+      }
+    },
+    hover: {
+      scale: 0.8,
+    },
   };
 
   const handles = [
@@ -26,19 +39,24 @@ const Profile = ({ member }: MemberProp) => {
 
   return (
     <div className="flex flex-col items-center m-4">
-      <motion.div
-        variants={holder}
-        whileHover="hover"
-        className="flex justify-center items-center rounded-full transform border-2 hover:border-base-teal hover:border-4 border-black p-4 relative"
-      >
-        <motion.img
-          variants={img}
-          alt={member.name}
-          className="rounded-full relative w-48 h-48 object-cover z-10 "
-          src={member.img_url}
-        />
-        <Socials handles={handles} />
-      </motion.div>
+      {typeof window !== "undefined" ? (
+        <motion.div
+          variants={holder}
+          whileHover="hover"
+          initial="initial"
+          className="flex justify-center items-center  w-56 h-56 rounded-full border-2 hover:border-base-teal hover:border-4 border-black p-4 relative"
+        >
+          <motion.img
+            variants={img}
+            alt={member.name}
+            className="rounded-full w-full h-full relative object-cover z-10 "
+            src={member.img_url}
+          />
+          <Socials handles={handles} />
+        </motion.div>
+      ) : (
+        <h3>Loading...</h3>
+      )}
       <h5 className="text-lg mt-6 font-medium">{member.name}</h5>
       <p className="text-sm">{`"${member.tagline}"`}</p>
     </div>
--- client/components/team/socials.tsx
+++ client/components/team/socials.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
-
-import { SocialIcon } from "../../utils/functions/social-icon";
+import { SocialIcon } from "../../utils/functions";
 
 interface SocialsProps {
   handles: [
@@ -17,20 +16,28 @@ const Socials = ({ handles }: SocialsProps) => {
   );
   const len = filteredHandles.length;
   const hypo = 95;
-  const closeness = 1;
+
   const angles = {
-    1: [0],
-    2: [(Math.PI / 8) * closeness, (-Math.PI / 8) * closeness],
-    3: [(Math.PI / 4) * closeness, 0, (-Math.PI / 4) * closeness],
-    4: [
-      (Math.PI / 4) * closeness,
-      (Math.PI / 8) * closeness,
-      0 * closeness,
-      (-Math.PI / 8) * closeness,
-    ],
+    1: [Math.PI / 4],
+    2: [Math.PI / 4, Math.PI / 8],
+    3: [Math.PI / 4, Math.PI / 8, 0],
+    4: [Math.PI / 4, Math.PI / 8, 0, -Math.PI / 8],
   };
   const jump = {
+    initial: (i) => {
+      if (window.innerWidth >= 768) {
+        console.log("In PC view", window.innerWidth);
+        return {};
+      }
+      const Yval = hypo * Math.sin(angles[len][i]);
+      const Xval = hypo * Math.cos(angles[len][i]);
+      return { x: Xval, y: Yval, scale: 1.1 };
+    },
     hover: (i) => {
+      if (window.innerWidth < 768) {
+        console.log("In Mobile view", window.innerWidth);
+        return {};
+      }
       const Yval = hypo * Math.sin(angles[len][i]);
       const Xval = hypo * Math.cos(angles[len][i]);
       return { x: Xval, y: Yval, scale: 1.1 };
@@ -39,17 +46,18 @@ const Socials = ({ handles }: SocialsProps) => {
 
   return (
     <>
-      {filteredHandles.map((handle, key) => (
-        <motion.div
-          key={key}
-          variants={jump}
-          custom={key}
-          className="w-4 h-4 absolute z-0"
-          transition={{ type: "string", default: { duration: 0.1 } }}
-        >
-          <SocialIcon handle={handle} />
-        </motion.div>
-      ))}
+      {typeof window !== "undefined" &&
+        filteredHandles.map((handle, key) => (
+          <motion.div
+            key={key}
+            variants={jump}
+            custom={key}
+            className="w-4 h-4 absolute z-0"
+            transition={{ type: "string", default: { duration: 0.1 } }}
+          >
+            <SocialIcon handle={handle} />
+          </motion.div>
+        ))}
     </>
   );
 };
