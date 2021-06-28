6db677bdc1e371fc7334dc75b114df540de55ae4
jagnani73 <yashjagnani73@gmail.com>
2021-06-28 07:56:35 +0530

    💄 footer

--- client/components/shared/footer.tsx
+++ client/components/shared/footer.tsx
@@ -1,20 +1,45 @@
 import { GitHubIcon, TwitterIcon, InstagramIcon } from "../../utils/icons";
 
-export default function Footer() {
-  const Socials = [<GitHubIcon />, <TwitterIcon />, <InstagramIcon />];
+const Footer = () => {
+  const socials: {
+    icon: JSX.Element;
+    href: string;
+  }[] = [
+    {
+      icon: <GitHubIcon />,
+      href: "https://github.com",
+    },
+    {
+      icon: <GitHubIcon />,
+      href: "https://github.com",
+    },
+    {
+      icon: <GitHubIcon />,
+      href: "https://github.com",
+    },
+  ];
+
   return (
-    <div className="bg-base-smoke flex flex-col md:flex-row justify-evenly items-center rounded-b-2xl  p-4 md:p-6 text-white">
+    <div className="bg-base-smoke flex flex-col md:flex-row justify-evenly items-center rounded-b-2xl p-4 md:p-6 text-white">
       <div className="w-1/3"></div>
-      <p className="w-1/3 text-center my-auto text-black font-thin mx-2">
+      <p className="w-1/3 text-center my-auto text-black mx-2">
         Something Witty
       </p>
       <div className="w-1/3 flex flex-row justify-evenly items-center">
-        {Socials.map((icon) => (
-          <div className="bg-base-blue flex justify-center items-center p-2 rounded-xl">
-            <div className="w-5 h-5">{icon}</div>
-          </div>
+        {socials.map((social) => (
+          <a
+            key={social.href}
+            href={social.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-base-teal flex justify-center items-center p-2 rounded-xl"
+          >
+            <span className="w-5 h-5">{social.icon}</span>
+          </a>
         ))}
       </div>
     </div>
   );
-}
+};
+
+export default Footer;
