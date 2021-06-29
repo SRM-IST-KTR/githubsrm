ae1e9171896cf684ceb0cca6648490da193ffe42
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-06-29 16:47:56 +0530

    ✨ Added sentry

--- server/githubsrm/core/settings.py
+++ server/githubsrm/core/settings.py
@@ -14,8 +14,17 @@ from pathlib import Path
 from dotenv import load_dotenv
 from corsheaders.defaults import default_headers
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[DjangoIntegration()]
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
