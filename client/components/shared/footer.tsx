e10cd11bd5e278b18392c8cdd9ba5abb3df05fc2
Sakshi <choudharysakshi023@gmail.com>
2021-06-29 15:05:50 +0530

    💄 footer

--- client/components/shared/footer.tsx
+++ client/components/shared/footer.tsx
@@ -4,6 +4,7 @@ import {
   InstagramIcon,
   LinkedinIcon,
 } from "../../utils/icons";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   const socials: {
@@ -30,21 +31,28 @@ const Footer = () => {
 
   return (
     <div className="bg-base-smoke flex flex-col md:flex-row justify-evenly items-center rounded-b-2xl p-2 md:p-6 text-white">
-      <div className="w-1/3 h-0 md:h-auto"></div>
-      <p className="w-full md:w-1/3 text-center my-2 md:my-auto text-black mx-2 min-w-max">
-        Something Witty
+      <div className="w-1/3 h-0 md:h-auto text-base-blue font-medium">
+        <a href="mailto:community@githubsrm.tech">community@githubsrm.tech</a>
+      </div>
+      <p className="w-full md:w-1/3 text-center my-2 md:my-auto text-gray-700 font-regular mx-2 min-w-max">
+        Developed with <span className="line-through">friends</span> Groovy
       </p>
       <div className="w-full md:w-1/3 flex flex-row justify-around md:justify-evenly items-center">
         {socials.map((social) => (
-          <a
+          <motion.div
             key={social.href}
-            href={social.href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-base-teal flex justify-center items-center p-2 rounded-xl"
+            whileHover={{ scale: 1.2, rotate: 360 }}
+            whileTap={{ scale: 1.3, rotate: -360, borderRadius: "100%" }}
           >
-            <span className="w-5 h-5">{social.icon}</span>
-          </a>
+            <a
+              href={social.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-base-teal flex justify-center items-center p-2 rounded-xl"
+            >
+              <span className="w-5 h-5">{social.icon}</span>
+            </a>
+          </motion.div>
         ))}
       </div>
     </div>
