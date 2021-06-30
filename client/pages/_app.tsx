ecb98b64f5b6b7e76e4b80bea47d9597317a8619
jagnani73 <yashjagnani73@gmail.com>
2021-06-30 05:45:33 +0530

    ✨ enable sentry

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,11 +6,11 @@ import { Integrations } from "@sentry/tracing";
 
 import "../styles/tailwind.styles.css";
 
-// Sentry.init({
-//   dsn: "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
-//   integrations: [new Integrations.BrowserTracing()],
-//   tracesSampleRate: 1.0,
-// });
+Sentry.init({
+  dsn: "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
