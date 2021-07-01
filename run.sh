2729c675cd32929d6402a9d4382b9dbf5a911c26
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-07-01 20:00:30 +0530

    ⚡️ improve bash scripts

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ ERROR_TEXT="\e[1;31m[ERROR]\e[0m"
 
 if [ $# -ne 1 ]
 then
-echo -e "${ERROR_TEXT} Correct Usage: ./run.sh <PORT>"
+echo -e "$ERROR_TEXT Correct Usage: ./run.sh <PORT>"
 exit 1
 fi
 
 # run the django server
 cd server/githubsrm/
-echo -e "${INFO_TEXT} Server starting on Port $1 [...]"
-$(gunicorn core.wsgi:application --bind 0.0.0.0:$1 --access-logfile - --error-logfile - --reload)
+echo -e "$INFO_TEXT Server starting on Port $1 [...]"
+gunicorn core.wsgi:application --bind 0.0.0.0:$1 --access-logfile - --error-logfile - --reload
--- run_tests.sh
+++ run_tests.sh
@@ -5,20 +5,20 @@ ERROR_TEXT="\e[1;31m[ERROR]\e[0m"
 
 if [ $# -ne 1 ]
 then
-echo -e "${ERROR_TEXT} Correct Usage: ./run_tests.sh <PORT>"
+echo -e "$ERROR_TEXT Correct Usage: ./run_tests.sh <PORT>"
 exit 1
 fi
 
 # run tests on the API
 cd server
-echo -e "${INFO_TEXT} Running Unit Tests [...]"
+echo -e "$INFO_TEXT Running Unit Tests [...]"
 python3 -m unittest tests/test_schema.py
-echo -e "${INFO_TEXT} Running Integration Tests [...]"
+echo -e "$INFO_TEXT Running Integration Tests [...]"
 python3 -m unittest tests/test_apis.py 
 # TEMPORARY FIX
 sleep 10
 # TEMPORARY FIX
 cd ..
-echo -e "${INFO_TEXT} Killing Django Proccess on Port $1 [...]"
-$(fuser -k $1/tcp)
-echo -e "${INFO_TEXT} Tests Run Successfully!"
+echo -e "$INFO_TEXT Killing Django Proccess on Port $1 [...]"
+fuser -k $1/tcp
+echo -e "$INFO_TEXT Tests Run Successfully!"
