926405aa52d6ca8b2e606d87dac2b1bf47456acf
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-07-01 19:28:25 +0530

    ✨ remove throttle from get

---
+++ server/githubsrm/apis/throttle.py
@@ -0,0 +1,10 @@
+from rest_framework import throttling
+
+
+class PostThrottle(throttling.AnonRateThrottle):
+    scope = 'post_throttle'
+
+    def allow_request(self, request, view):
+        if request.method == 'GET':
+            return True
+        return super().allow_request(request, view)
