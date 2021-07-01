b03399d1e76914ce72d7f3c9809b2499a2cc1381
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-07-01 22:00:11 +0530

    Dev (#15)
    💚 update CI scripts

--- test_runner.sh
+++ test_runner.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-parallel -j 2 :::: test.config
+
+INFO_TEXT="\e[1;32m[INFO]\e[0m"
+ERROR_TEXT="\e[1;31m[ERROR]\e[0m"
+
+if [ "$1" = "frontend" ]
+then
+echo -e "$INFO_TEXT Running Front-end Tests [...]"
+parallel -j 2 --ungroup :::: test.frontend.config
 exit 0
+fi
+if [ "$1" = "backend" ]
+then
+echo -e "$INFO_TEXT Running Back-end Tests [...]"
+parallel -j 2 --ungroup :::: test.backend.config
+exit 0
+fi
+echo -e "$ERROR_TEXT Correct Usage: ./test_runner.sh <backend|frontend>"
+exit 1
