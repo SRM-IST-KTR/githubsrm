eaac038ca71c1f0fa8546649b6749eaa5ef10dd3
Shivam Shekhar <shvam0000@gmail.com>
2021-07-09 00:02:14 +0530

    ✨ maintainer portal

---
+++ client/components/portals/maintainer/dashboard/card.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const card = () => {
+  return <div></div>;
+};
+
+export default card;
---
+++ client/components/portals/maintainer/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const login = () => {
+  return <div></div>;
+};
+
+export default login;
