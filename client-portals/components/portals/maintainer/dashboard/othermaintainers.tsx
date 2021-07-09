c162cd18389682001e1f846f3a26bdc97cba5cc2
Shivam Shekhar <shvam0000@gmail.com>
2021-07-09 23:45:22 +0530

    ✨ other maintainers

---
+++ client-portals/components/portals/maintainer/dashboard/othermaintainers.tsx
@@ -0,0 +1,31 @@
+const OtherMaintainers = () => {
+  const otherMaintainers = [
+    {
+      name: "Sebastian",
+      github: "https://github.com/sebastian-bergmann",
+    },
+    {
+      name: "Sebastian Bergmann",
+      github: "https://github.com/sebastianbergmann",
+    },
+    {
+      name: "Sebastian",
+      github: "https://github.com/sebastian-bergmann",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="text-white font-extrabold text-3xl">Other Maintainers</h1>
+      <ul>
+        {otherMaintainers.map((maintainer) => (
+          <li className="text-2xl text-white list-disc">
+            <a href={maintainer.github}>{maintainer.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default OtherMaintainers;
