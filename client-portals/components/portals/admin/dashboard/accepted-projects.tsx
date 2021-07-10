52c669e90e35eb997616d726953535e7930e5098
Shivam Shekhar <shvam0000@gmail.com>
2021-07-10 13:03:36 +0530

    ✨ admin - accepted projects

---
+++ client-portals/components/portals/admin/dashboard/accepted-projects.tsx
@@ -0,0 +1,11 @@
+import Adminnavbar from "./navbar";
+
+const AcceptedProjectsCard = () => {
+  return (
+    <div>
+      <Adminnavbar />
+    </div>
+  );
+};
+
+export default AcceptedProjectsCard;
---
+++ client-portals/pages/admin/dashboard/accepted-projects.tsx
@@ -0,0 +1,10 @@
+import AcceptedProjectsCard from "../../../components/portals/admin/dashboard/accepted-projects";
+const AcceptedProjects = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-base-blue">
+      <AcceptedProjectsCard />
+    </div>
+  );
+};
+
+export default AcceptedProjects;
