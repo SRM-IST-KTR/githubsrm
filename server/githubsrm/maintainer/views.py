2c21c7f1174acbca799e30b901fb09d130f334d0
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-07-11 04:58:00 +0530

    ✨ Added contributor approve route

--- server/githubsrm/maintainer/views.py
+++ server/githubsrm/maintainer/views.py
@@ -1,29 +1,56 @@
-from django.shortcuts import render
-
+from django.http.response import JsonResponse
 from .utils import project_SingleProject, Projects_pagnation
-
-from rest_framework import response, status
+from .definitions import MaintainerSchema
+from rest_framework import status
 from rest_framework.views import APIView
-
-
-# Create your views here.
+from maintainer import entry
 
 
 class Projects(APIView):
 
-    def get(self, request, **kwargs):
+    def post(self, request, **kwargs) -> JsonResponse:
+        """Accept contributors. 
 
+        Args:
+            request
+
+        Returns:
+            JsonResponse
+        """
+        validate = MaintainerSchema(request.data).valid()
+        if 'error' in validate:
+            return JsonResponse(data=validate, status=400)
+
+        if entry.approve_contributor(validate.get("project_id"), validate.get("contributor_id")):
+            return JsonResponse(data={
+                "approved contributor": True
+            }, status=200)
+
+        return JsonResponse(data={
+            "error": "invalid ids or contributor already approved"
+        }, status=400)
+
+    def get(self, request, **kwargs) -> JsonResponse:
+        """Projects get view for maintainer portal.
+
+        Args:
+            request
+
+        Returns:
+            JsonResponse
+        """
         Pagination = ['page']
-
         SingleProject = ['projectId', 'maintainer', 'contributor']
 
         RequestQueryKeys = list(request.GET.keys())
 
         if len(set(Pagination) & set(RequestQueryKeys)) == 1:
-            return response.Response(Projects_pagnation(request, **kwargs), status=status.HTTP_200_OK)
+            return JsonResponse(Projects_pagnation(request, **kwargs), status=status.HTTP_200_OK)
 
         elif len(set(SingleProject) & set(RequestQueryKeys)) == 3:
-            return response.Response(project_SingleProject(request, **kwargs), status=status.HTTP_200_OK)
+            return JsonResponse(project_SingleProject(request, **kwargs), status=status.HTTP_200_OK)
 
         else:
-            return response.Response("Query Params are different from expected", status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(data={
+                "error": "invalid query parameters"
+            }, status=status.HTTP_400_BAD_REQUEST)
