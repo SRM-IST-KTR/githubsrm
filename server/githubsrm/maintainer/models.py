9b664c8a24cb340cf84dcf89628e4ee1b7b4774a
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-07-11 04:57:33 +0530

    🚩 updated entry flow

--- server/githubsrm/maintainer/models.py
+++ server/githubsrm/maintainer/models.py
@@ -1,13 +1,43 @@
 
 import pymongo
 from django.conf import settings
-# Create your models here.
 
 
 class Entry:
     db = None
 
-    def __init__(self):
+    def __init__(self) -> None:
         client = pymongo.MongoClient(settings.DATABASE['mongo_uri'])
         self.db = client[settings.DATABASE['db']]
-        
+
+    def approve_contributor(self, project_id: str, contributor_id: str) -> bool:
+        """Maintainer approve contributor
+
+        Args:
+            project_id (str): project id
+            contributor_id (str): contributor id
+
+        Returns:
+            bool
+        """
+
+        contributor = self.db.contributor.find_one_and_update(
+            {"_id": contributor_id, "interested_project": project_id},
+            update={
+                "$set": {"is_maintainer_approved": True}
+            }
+        )
+
+        if contributor:
+            if contributor.get("is_maintainer_approved") and contributor.get("is_admin_approved"):
+                return False
+            self.db.project.find_one_and_update(
+                {"_id": project_id},
+                update={
+                    "$addToSet": {
+                        "contributor_id": contributor_id
+                    }
+                }
+            )
+
+        return False
