341c4ab5477723644729fbc0cff8f623379ee182
saxenabhishek <as7122000@gmail.com>
2021-07-11 02:00:48 +0530

    🚑  model error

--- server/githubsrm/maintainer/models.py
+++ server/githubsrm/maintainer/models.py
@@ -1,10 +1,12 @@
 
 import pymongo
 from django.conf import settings
-from pymongo.database import Database
 # Create your models here.
 
+
 class Entry:
-    def __init___(self):
+    db = None
+
+    def __init__(self):
         client = pymongo.MongoClient(settings.DATABASE['mongo_uri'])
         self.db = client[settings.DATABASE['db']]
