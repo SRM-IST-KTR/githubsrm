470bf7e433dd3c1882f592b4ee00357450eeb457
Shivam Shekhar <shvam0000@gmail.com>
2021-07-11 21:20:53 +0530

    ⚡️auth login

--- client-portals/components/portals/admin/dashboard/dashboard.tsx
+++ client-portals/components/portals/admin/dashboard/dashboard.tsx
@@ -1,13 +1,35 @@
 import AcceptedProjectsCard from "./accepted-projects";
 import Adminnavbar from "./navbar";
+import React, { useState, useEffect } from "react";
+import AuthContext from "../../../../services/auth-context";
 
 const AcceptedProjectDashboard = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = sessionStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
   return (
-    <div className="min-h-screen p-14 bg-base-blue">
-      <Adminnavbar />
-      <h2 className="text-4xl font-extrabold text-white mb-10">Hi, Admin</h2>
-      <AcceptedProjectsCard />
-    </div>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      {!isLoggedIn && <p>not logged in</p>}
+      {isLoggedIn && (
+        <div className="min-h-screen p-14 bg-base-blue">
+          <Adminnavbar />
+          <h2 className="text-4xl font-extrabold text-white mb-10">
+            Hi, Admin
+          </h2>
+          <AcceptedProjectsCard />
+        </div>
+      )}
+    </AuthContext.Provider>
   );
 };
 
--- client-portals/components/portals/admin/register.tsx
+++ client-portals/components/portals/admin/register.tsx
@@ -1,5 +1,5 @@
 import { Formik, Form, Field } from "formik";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AdminRegisterData } from "../../../utils/interfaces";
 import {
   adminRegisterValidation,
@@ -8,9 +8,19 @@ import {
 import { Input } from "../../shared";
 import instance from "../../../services/api";
 import { successToast, errToast } from "../../../utils/functions/toast";
+import AuthContext from "../../../services/auth-context";
 
 const AdminRegister = () => {
-  const [token, setToken] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [authToken, setAuthToken] = useState("");
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = sessionStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const initialValues: { email: string; password: string } = {
     email: "",
@@ -20,7 +30,7 @@ const AdminRegister = () => {
   const submitValues = (values: AdminRegisterData) => {
     instance
       .post("/admin/register", values, {
-        headers: { Authorization: `Bearer ${token}` },
+        headers: { Authorization: `Bearer ${authToken}` },
       })
       .then((res) => {
         successToast("Admin registered successfully!");
@@ -31,40 +41,49 @@ const AdminRegister = () => {
   };
 
   return (
-    <div className="min-h-screen p-14 bg-base-blue">
-      <h1 className="flex justify-center text-4xl font-extrabold text-white">
-        Admin Registration
-      </h1>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      {isLoggedIn && <p>register page -already logged in</p>}
+      {!isLoggedIn && (
+        <div className="min-h-screen p-14 bg-base-blue">
+          <h1 className="flex justify-center text-4xl font-extrabold text-white">
+            Admin Registration
+          </h1>
 
-      <Formik
-        initialValues={initialValues}
-        onSubmit={submitValues}
-        validationSchema={adminRegisterValidation}
-      >
-        <Form className="flex flex-col px-6 lg:w-1/4 max-w-6xl mt-10 py-6 mx-auto bg-white rounded-lg">
-          {adminRegisterInputs.map((input) => (
-            <div className="border-2 border-gray-700 rounded my-4 p-4">
-              <Input key={input.id} {...input} />
-            </div>
-          ))}
-          <Field
-            onChange={(e) => setToken(e.target.value)}
-            className="border-2 border-gray-800 rounded-md p-3 mt-3"
-            type="password"
-            name="token"
-            placeholder="Secret Key"
-          />
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="rounded-xl font-bold items-center my-3 bg-base-teal p-3"
-            >
-              Register
-            </button>
-          </div>
-        </Form>
-      </Formik>
-    </div>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={submitValues}
+            validationSchema={adminRegisterValidation}
+          >
+            <Form className="flex flex-col px-6 lg:w-1/4 max-w-6xl mt-10 py-6 mx-auto bg-white rounded-lg">
+              {adminRegisterInputs.map((input) => (
+                <div className="border-2 border-gray-700 rounded my-4 p-4">
+                  <Input key={input.id} {...input} />
+                </div>
+              ))}
+              <Field
+                onChange={(e) => setAuthToken(e.target.value)}
+                className="border-2 border-gray-800 rounded-md p-3 mt-3"
+                type="password"
+                name="token"
+                placeholder="Secret Key"
+              />
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="rounded-xl font-bold items-center my-3 bg-base-teal p-3"
+                >
+                  Register
+                </button>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      )}
+    </AuthContext.Provider>
   );
 };
 
