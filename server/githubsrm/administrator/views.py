4756997fd0193d247fe7aab8c1752256650308c2
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-07-12 01:35:55 +0530

    ✨ Added reCaptcha

--- server/githubsrm/administrator/views.py
+++ server/githubsrm/administrator/views.py
@@ -1,5 +1,6 @@
 
-from apis import service
+from botocore.serialize import JSONSerializer
+from apis import service, check_token
 from apis import PostThrottle
 from django.http.response import JsonResponse
 from rest_framework import status
@@ -11,6 +12,7 @@ from .definitions import AdminSchema, ApprovalSchema
 from .perms import AuthAdminPerms
 from .utils import project_Pagination, project_SingleProject
 from apis import service
+
 
 class RegisterAdmin(APIView):
     permission_classes = [AuthAdminPerms]
@@ -25,20 +27,33 @@ class RegisterAdmin(APIView):
         Returns:
             JsonResponse
         """
-        valid = AdminSchema(request.data).valid()
 
-        if 'error' in valid:
+        try:
+            reCaptcha = request.META.get("HTTP_X_RECAPTCHA_TOKEN")
+        except KeyError as e:
             return JsonResponse(data={
-                "error": valid
-            }, status=400)
+                "error": "reCaptcha token not provided"
+            }, status=401)
 
-        if entry.insert_admin(request.data):
+        if check_token(reCaptcha):
+
+            valid = AdminSchema(request.data).valid()
+
+            if 'error' in valid:
+                return JsonResponse(data={
+                    "error": valid
+                }, status=400)
+
+            if entry.insert_admin(request.data):
+                return JsonResponse(data={
+                    "registered": True
+                }, status=200)
             return JsonResponse(data={
-                "registered": True
-            }, status=200)
+                "error": "invalid data / user exists"
+            }, status=status.HTTP_400_BAD_REQUEST)
         return JsonResponse(data={
-            "error": "invalid data / user exists"
-        }, status=status.HTTP_400_BAD_REQUEST)
+            "error": "Invalid recaptcha token"
+        }, status=401)
 
 
 class AdminLogin(APIView):
@@ -56,22 +71,36 @@ class AdminLogin(APIView):
         Returns:
             JsonResponse
         """
-        validate = AdminSchema(request.data).valid()
-        if 'error' in validate:
-            return JsonResponse(status=400)
-        password = validate.get('password')
-        if entry.verify_admin(email=validate.get('email'), password=password):
-            keys = jwt_keys.issue_key(payload={
-                "admin": True,
-                "user": validate.get('email')
-            })
-            if keys:
-                return JsonResponse(data={
-                    "keys": keys
-                }, status=200)
-            return JsonResponse({"error":"ISR?"},status=500)
+
+        try:
+            reCaptcha = request.META.get("HTTP_X_RECAPTCHA_TOKEN")
+        except Exception as e:
+            return JsonResponse(data={
+                "error": "reCaptcha token not provided"
+            }, status=401)
+
+        if check_token(reCaptcha):
+
+            validate = AdminSchema(request.data).valid()
+            if 'error' in validate:
+                return JsonResponse(status=400)
+            password = validate.get('password')
+            if entry.verify_admin(email=validate.get('email'), password=password):
+                keys = jwt_keys.issue_key(payload={
+                    "admin": True,
+                    "user": validate.get('email')
+                })
+                if keys:
+                    return JsonResponse(data={
+                        "keys": keys
+                    }, status=200)
+                return JsonResponse({"error": "ISR?"}, status=500)
+            return JsonResponse(data={
+                "error": "invalid password"
+            }, status=401)
+
         return JsonResponse(data={
-            "error": "invalid password"
+            "error": "Invalid recaptcha token"
         }, status=401)
 
 
@@ -112,8 +141,9 @@ class ProjectsAdmin(APIView):
                 if len(value['maintainer_id']) == 1:
                     #! Alpha maintainer flow
                     if existing:
-                        # ? service.wrapper_email() send conformation emails.
-                        service.wrapper_email(role="existing_alpha_maintainer",data=None)
+
+                        # service.wrapper_email(
+                        # role="existing_alpha_maintainer", data=None)
                         return JsonResponse(data={
                             "success": "Approved existing maintainer"
                         }, status=200)
@@ -122,9 +152,8 @@ class ProjectsAdmin(APIView):
                             identifier=validate.get("maintainer_id"))
                         print(password)
 
-                        # ? service.wrapper_email() send conformation emails with password.
-                        service.wrapper_email(
-                            role="alpha_maintainer_w_password", data=None)
+                        # service.wrapper_email(
+                        #     role="alpha_maintainer_w_password", data=None)
                         return JsonResponse(data={
                             "success": "Approved new maintainer"
                         }, status=200)
@@ -132,12 +161,11 @@ class ProjectsAdmin(APIView):
                 if len(value['maintainer_id']) > 1:
                     #! Beta maintainer flow
                     if existing:
-                        # ? service.wrapper_email send acceptance email to beta maintainer
-                        # ? Alpha maintainer gets an email of beta maintainer's approval
-                        service.wrapper_email(
-                            role="beta_maintainer_approval", data=None)
-                        service.wrapper_email(
-                            role="beta_maintainer_approval_to_alpha", data=None)
+
+                        # service.wrapper_email(
+                        #     role="beta_maintainer_approval", data=None)
+                        # service.wrapper_email(
+                        #     role="beta_maintainer_approval_to_alpha", data=None)
                         return JsonResponse(data={
                             "Approved existing maintainer": True
                         }, status=200)
@@ -145,12 +173,11 @@ class ProjectsAdmin(APIView):
                     else:
                         password = entry.get_random_password(
                             identifier=validate.get("maintainer_id"))
-                        # ? service.wrapper_email send acceptance email to beta maintainer with password
-                        # ? Alpha maintainer gets an email of beta maintainer's approval
-                        service.wrapper_email(
-                            role="beta_maintainer_approval_w_password", data=None)
-                        service.wrapper_email(
-                            role="beta_maintainer_approval_to_alpha", data=None)
+
+                        # service.wrapper_email(
+                        #     role="beta_maintainer_approval_w_password", data=None)
+                        # service.wrapper_email(
+                        # role="beta_maintainer_approval_to_alpha", data=None)
                         print(password)
                         return JsonResponse(data={
                             "Approved new maintainer": True
@@ -165,9 +192,8 @@ class ProjectsAdmin(APIView):
                                      project_url=validate.get("project_url"),
                                      private=validate.get("private")):
 
-                # ? service.wrapper send maintainer approval conformation
-                service.wrapper_email(
-                    role="approve_project", data=None)
+                # service.wrapper_email(
+                #     role="approve_project", data=None)
                 return JsonResponse(data={
                     "Approved Project": validate.get("project_id")
                 }, status=200)
--- server/githubsrm/maintainer/views.py
+++ server/githubsrm/maintainer/views.py
@@ -1,7 +1,7 @@
 from hashlib import sha256
 
 from administrator.issue_jwt import IssueKey
-from apis import PostThrottle
+from apis import PostThrottle, check_token
 from django.http.response import JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
@@ -30,19 +30,33 @@ class Projects(APIView):
             JsonResponse
         """
 
-        validate = MaintainerSchema(request.data, path=request.path).valid()
-
-        if 'error' in validate:
-            return JsonResponse(data=validate, status=400)
-
-        if entry.approve_contributor(validate.get("project_id"), validate.get("contributor_id")):
+        try:
+            reCaptcha = request.META.get("HTTP_X_RECAPTCHA_TOKEN")
+        except KeyError as e:
             return JsonResponse(data={
-                "approved contributor": True
-            }, status=200)
+                "error": "recaptcha not provided"
+            }, status=401)
+
+        if check_token(reCaptcha):
+
+            validate = MaintainerSchema(
+                request.data, path=request.path).valid()
+
+            if 'error' in validate:
+                return JsonResponse(data=validate, status=400)
+
+            if entry.approve_contributor(validate.get("project_id"), validate.get("contributor_id")):
+                return JsonResponse(data={
+                    "approved contributor": True
+                }, status=200)
+
+            return JsonResponse(data={
+                "error": "invalid ids or contributor already approved"
+            }, status=400)
 
         return JsonResponse(data={
-            "error": "invalid ids or contributor already approved"
-        }, status=400)
+            "error": "Invalid reCaptcha token"
+        }, status=401)
 
     def get(self, request, **kwargs) -> JsonResponse:
         """Projects get view for maintainer portal.
@@ -79,28 +93,50 @@ class Login(APIView):
         Login route get email and password make jwt and send.
         """
 
-        validate = MaintainerSchema(request.data, path=request.path).valid()
-        if 'error' in validate:
-            return JsonResponse(data={"error": validate.get("error")}, status=400)
+        try:
+            reCaptcha = request.META.get("HTTP_X_RECAPTCHA_TOKEN")
+        except KeyError as e:
+            return JsonResponse(data={
+                "error": "recaptcha not provided"
+            }, status=401)
 
-        password_hashed = sha256(request.data["password"].encode()).hexdigest()
-        doc_list_iter = entry.Send_all_Maintainer_email(request.data["email"])
-        doc_list = [i for i in doc_list_iter]
+        if check_token(reCaptcha):
+            validate = MaintainerSchema(
+                request.data, path=request.path).valid()
+            if 'error' in validate:
+                return JsonResponse(data={
+                    "error": validate.get("error")
+                }, status=400)
 
-        if len(doc_list):
-            if doc_list[0]["password"] != password_hashed:
-                return JsonResponse({"message": "wrong password"},
-                                    status=status.HTTP_401_UNAUTHORIZED)
+            password_hashed = sha256(
+                request.data["password"].encode()).hexdigest()
+            doc_list_iter = entry.Send_all_Maintainer_email(
+                request.data["email"])
+            doc_list = [i for i in doc_list_iter]
 
-            payload = {}
-            payload["email"] = doc_list[0]["email"]
-            payload["name"] = doc_list[0]["name"]
-            payload["project_id"] = [i["project_id"] for i in doc_list]
+            if len(doc_list):
+                if doc_list[0]["password"] != password_hashed:
+                    return JsonResponse(data={
+                        "message": "wrong password"
+                    }, status=status.HTTP_401_UNAUTHORIZED)
 
-            if jwt := key.issue_key(payload):
-                return JsonResponse({"key": jwt}, status=status.HTTP_200_OK)
+                payload = {}
+                payload["email"] = doc_list[0]["email"]
+                payload["name"] = doc_list[0]["name"]
+                payload["project_id"] = [i["project_id"] for i in doc_list]
 
-        return JsonResponse({"message": "Does not exist"}, status=status.HTTP_401_UNAUTHORIZED)
+                if jwt := key.issue_key(payload):
+                    return JsonResponse(data={
+                        "key": jwt
+                    }, status=status.HTTP_200_OK)
+
+            return JsonResponse(data={
+                "message": "Does not exist"
+            },  status=status.HTTP_401_UNAUTHORIZED)
+
+        return JsonResponse(data={
+            "error": "Invalid recaptcha token"
+        }, status=401)
 
 
 class ResetPassword(APIView):
