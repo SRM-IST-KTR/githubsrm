84be654456cc779375c07660fe8a52ae7088a9ea
Shivam Shekhar <shvam0000@gmail.com>
2021-07-17 21:17:55 +0530

    🐛 remove comments and conditional statement

--- client-portals/components/admin/dashboard/index.tsx
+++ client-portals/components/admin/dashboard/index.tsx
@@ -30,30 +30,6 @@ const ProjectApplications = () => {
   ];
 
   const acceptProjectHandler = async (project_id, isprivate, project_url) => {
-    // const recaptchaToken = await getRecaptchaToken("post");
-    // const token = sessionStorage.getItem("token");
-    // await instance
-    //   .post(
-    //     `admin/projects?projectId=${project_id}&role=project`,
-    //     {
-    //       project_id: project_id,
-    //       private: isprivate,
-    //       project_url: project_url,
-    //     },
-    //     {
-    //       headers: {
-    //         Authorization: `Bearer ${token}`,
-    //         "X-RECAPTCHA-TOKEN": recaptchaToken,
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     setAccepted(true);
-    //     successToast("Project Approved sucessfully!");
-    //   })
-    //   .catch((err) => {
-    //     errToast(err.message);
-    //   });
     setLoading(true);
     const res = await postAcceptProjectHandler(
       project_id,
@@ -63,8 +39,6 @@ const ProjectApplications = () => {
     if (res) {
       setAccepted(true);
       successToast("Project Approved successfully!");
-    } else {
-      errToast("Error, Please Try Again!");
     }
   };
 
