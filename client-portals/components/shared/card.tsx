877ecc9cf31b5e1bd62b728548b3cb18f79ac146
Sakshi <choudharysakshi023@gmail.com>
2021-07-19 21:45:48 +0530

    💄 UI improvements (maintainer)

--- client-portals/components/shared/card.tsx
+++ client-portals/components/shared/card.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Card = ({ name, desc, url }) => {
   return (
-    <div className="md:w-3/4 w-full px-8 py-4 mx-auto bg-white rounded-lg shadow-md mt-6">
+    <div className="md:w-4/5 w-full px-8 py-8 mx-auto bg-white rounded-lg shadow-md mt-6">
       <div className="mt-2">
         <p className="text-2xl font-bold text-gray-700">{name}</p>
         <p className="mt-2 text-gray-600 w-6/7 overflow-auto no-scrollbar word-wrap">
