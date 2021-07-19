80f31a4305ce95576c6cf02a1eb9719ea3d8b693
Sakshi <choudharysakshi023@gmail.com>
2021-07-19 21:55:19 +0530

    💄 edge cases UI fix

--- client-portals/components/maintainer/dashboard/othermaintainers.tsx
+++ client-portals/components/maintainer/dashboard/othermaintainers.tsx
@@ -6,29 +6,30 @@ interface otherMaintainersProps {
 
 const OtherMaintainers = ({ otherMaintainers }: otherMaintainersProps) => {
   return (
-    <div className="my-10">
-      <h1 className="text-white font-bold text-2xl flex">
+    <div className="my-10 flex justify-evenly">
+      <h1 className="text-white font-bold text-xl flex">
         {otherMaintainers?.length > 0
-          ? "Other Maintainers -"
-          : "No other maintainers"}
+          ? "Other Maintainers :"
+          : "There are no other maintainers in this project"}
       </h1>
-
-      {otherMaintainers?.length > 0 &&
-        otherMaintainers?.map((maintainer) => (
-          <p
-            key={maintainer.github}
-            className="text-2xl cursor-pointer text-white list-disc"
-          >
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={maintainer.github}
-              className="hover:text-gray-600"
+      <ul className="list-disc">
+        {otherMaintainers?.length > 0 &&
+          otherMaintainers?.map((maintainer) => (
+            <li
+              key={maintainer.github}
+              className="text-2xl cursor-pointer text-white "
             >
-              {maintainer.name}
-            </a>
-          </p>
-        ))}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={maintainer.github}
+                className="hover:text-gray-600"
+              >
+                {maintainer.name}
+              </a>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
