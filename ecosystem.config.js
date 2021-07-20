a2a5a517b29bbe9a91eb84c9795070ae39b09866
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-07-21 02:16:35 +0530

    🚀 PM2 scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,5 +4,15 @@ module.exports = {
       name: "GitHubSRM: Django Server",
       script: "chmod +x ./run.sh && ./run.sh 8000",
     },
+    {
+      name: "GitHubSRM: Landing Page",
+      cwd: "client",
+      script: "yarn start:landing -p 3000",
+    },
+    {
+      name: "GitHubSRM: Portal Page",
+      cwd: "client",
+      script: "yarn start:portal -p 5000",
+    },
   ],
 };
