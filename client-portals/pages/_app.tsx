8cb162544b58e4f92ddfa409befdd1e0decf87bd
Shivam Shekhar <shvam0000@gmail.com>
2021-07-20 14:22:49 +0530

    💥 Enable Sentry

--- client-portals/pages/_app.tsx
+++ client-portals/pages/_app.tsx
@@ -2,15 +2,15 @@ import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AuthContextProvider from "../context/authContext";
-// import * as Sentry from "@sentry/react";
-// import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "../styles/tailwind.styles.css";
 
-// Sentry.init({
-//   dsn: "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
-//   integrations: [new Integrations.BrowserTracing()],
-//   tracesSampleRate: 1.0,
-// });
+Sentry.init({
+  dsn: "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
