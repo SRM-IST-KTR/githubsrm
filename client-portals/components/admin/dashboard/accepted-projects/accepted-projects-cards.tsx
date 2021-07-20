8d2d24d4dd524264557aa0da2fa39c1bd6589ffe
Sakshi <choudharysakshi023@gmail.com>
2021-07-20 19:35:42 +0530

    ✨ Pagination button comp

--- client-portals/components/admin/dashboard/accepted-projects/accepted-projects-cards.tsx
+++ client-portals/components/admin/dashboard/accepted-projects/accepted-projects-cards.tsx
@@ -2,8 +2,7 @@ import Card from "../../../shared/card";
 import React, { useState, useEffect } from "react";
 import { getAcceptedProjects } from "../../../../services/api";
 import CSSLoader from "../../../shared/loader";
-import Next from "../../../../utils/icons/next";
-import Previous from "../../../../utils/icons/previous";
+import PaginationButtons from "../../../shared/pagination-buttons";
 
 const AcceptedProjectsCards = () => {
   const [acceptedProjects, setAcceptedProjects] = useState([]);
@@ -39,36 +38,12 @@ const AcceptedProjectsCards = () => {
           />
         ))}
       </div>
-
-      <div className="fixed inline-flex w-full bottom-0 left-1/2 mb-32">
-        <button
-          disabled={!hasPrevPage}
-          className={`${
-            !hasPrevPage
-              ? "opacity-10 cursor-not-allowed"
-              : "hover:bg-base-green focus:bg-base-green"
-          } p-3 rounded-full`}
-          onClick={() => setPageNo(pageNo - 1)}
-        >
-          <span className="text-2xl font-extrabold">
-            <Previous />
-          </span>
-        </button>
-        <h2 className="text-gray-50 text-4xl  font-medium mx-3">{pageNo}</h2>
-        <button
-          disabled={!hasNextPage}
-          className={`${
-            !hasNextPage
-              ? "opacity-10 cursor-not-allowed"
-              : "hover:bg-base-green focus:bg-base-green"
-          } p-3 rounded-full`}
-          onClick={() => setPageNo(pageNo + 1)}
-        >
-          <span className="text-2xl font-extrabold">
-            <Next />
-          </span>
-        </button>
-      </div>
+      <PaginationButtons
+        hasNextPage={hasNextPage}
+        hasPrevPage={hasPrevPage}
+        pageNo={pageNo}
+        setPageNo={setPageNo}
+      />
     </>
   ) : (
     <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-base-blue">
