bed4434efa275d1b919e74853f0aadebf0426a30
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-07-21 01:37:03 +0530

    🚧 deployment

--- client-portals/next.config.js
+++ client-portals/next.config.js
@@ -1,3 +1,19 @@
+const { API_HOSTNAME } = process.env;
 module.exports = {
-  basePath: "/portal",
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+      {
+        source: "/api",
+        destination: `${API_HOSTNAME}/api`,
+      },
+      {
+        source: "/api/:path*",
+        destination: `${API_HOSTNAME}/api/:path*`,
+      },
+    ];
+  },
 };
