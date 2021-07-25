6343673339926901c643e884d7dbd70198cdc4c7
Sakshi <choudharysakshi023@gmail.com>
2021-07-26 01:25:48 +0530

    🚧 OAuth2.0

--- client/client-portals/pages/_app.tsx
+++ client/client-portals/pages/_app.tsx
@@ -6,12 +6,12 @@ import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import "../styles/tailwind.styles.css";
 
-Sentry.init({
-  dsn:
-    "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn:
+//     "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
+//   integrations: [new Integrations.BrowserTracing()],
+//   tracesSampleRate: 1.0,
+// });
 
 function MyApp({ Component, pageProps }) {
   return (
