18e589e29862335c82fbc2bfcff4f185436db6f8
Sakshi <choudharysakshi023@gmail.com>
2021-07-26 21:14:21 +0530

    🚑 fix invalid hook call

--- client/client-portals/components/maintainer/dashboard/dashboard.tsx
+++ client/client-portals/components/maintainer/dashboard/dashboard.tsx
@@ -8,6 +8,7 @@ import { errToast } from "utils/functions/toast";
 import { AuthContext } from "context/authContext";
 
 const index = () => {
+  const authContext = useContext(AuthContext);
   const [projects, setProjects] = useState<MaintainerProjectsProps[]>([]);
   const [accepted, setAccepted] = useState<boolean>(false);
   const [pageNo, setPageNo] = useState<number>(1);
@@ -22,7 +23,6 @@ const index = () => {
         API = await instance();
       } catch (error) {
         errToast("Session Expired! Please Login again!");
-        const authContext = useContext(AuthContext);
         authContext.logoutHandler();
       } finally {
         API?.get(`maintainer/projects?page=${pageNo}`)
