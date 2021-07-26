86069ba2d78318c67b83037ef8d117c9b4fa57b8
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-07-26 22:22:34 +0530

    ✅ Added single project pagination tests.
    Co-authored-by: Riju Mukherjee <rmukh561@gmail.com>

--- server/run_backend_tests.sh
+++ server/run_backend_tests.sh
@@ -1,6 +1,8 @@
 echo "[STARTING-DJANGO-SERVER]"
 python3 githubsrm/manage.py runserver &
 
+sleep 5
+
 python3 -m unittest tests/test_apis.py
 python3 -m unittest tests/test_admin.py
 python3 -m unittest tests/test_maintainer.py
