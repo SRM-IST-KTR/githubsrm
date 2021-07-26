6e0357aee48d0a496c882a444bedf3dd6a91bc4a
Shivam Shekhar <shvam0000@gmail.com>
2021-07-27 00:12:00 +0530

    🐛 Other contributors UI

--- client/client-portals/components/maintainer/dashboard/other-maintainers.tsx
+++ client/client-portals/components/maintainer/dashboard/other-maintainers.tsx
@@ -7,14 +7,14 @@ interface otherMaintainersProps {
 const OtherMaintainers = ({ otherMaintainers }: otherMaintainersProps) => {
   return (
     <div className="md:my-7 my-10 flex">
-      <h1 className="text-gray-200 font-medium text-xl flex mr-3">
+      <h1 className="text-gray-200 font-semibold text-2xl flex mr-3">
         {otherMaintainers?.length > 0
           ? "Other Maintainers :"
           : "There are no other maintainers in this project"}
       </h1>
 
       {otherMaintainers?.length > 0 &&
-        otherMaintainers?.map((maintainer) => (
+        otherMaintainers?.map((maintainer, index) => (
           <p
             key={maintainer.github}
             className="text-2xl cursor-pointer text-gray-100 "
@@ -23,9 +23,12 @@ const OtherMaintainers = ({ otherMaintainers }: otherMaintainersProps) => {
               target="_blank"
               rel="noopener noreferrer"
               href={maintainer.github}
-              className="hover:text-gray-600"
+              className="hover:text-base-green"
             >
-              {maintainer.name}
+              {maintainer.name}{" "}
+              {index !== otherMaintainers?.length - 1 && (
+                <span className="text-2xl">|&nbsp;</span>
+              )}
             </a>
           </p>
         ))}
