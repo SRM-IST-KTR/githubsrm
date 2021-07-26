255ca984f740d119bfbfa96c917372dc752af724
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-07-26 13:07:46 +0530

    🐛 Fixes Sentry Disable on Dev #118

--- client/client-portals/pages/_app.tsx
+++ client/client-portals/pages/_app.tsx
@@ -6,12 +6,20 @@ import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import "../styles/tailwind.styles.css";
 
-// Sentry.init({
-//   dsn:
-//     "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
-//   integrations: [new Integrations.BrowserTracing()],
-//   tracesSampleRate: 1.0,
-// });
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://db29973e199247598f5b3e72b8ff3296@o889865.ingest.sentry.io/5838970",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+  console.log(
+    "\x1b[1m\x1b[32mServer in Production Mode. Sentry Enabled.\x1b[0m"
+  );
+} else {
+  console.log(
+    "\x1b[1m\x1b[33mServer in Development Mode. Disabling Sentry.\x1b[0m"
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   return (
