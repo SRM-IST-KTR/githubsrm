3109167a14a2daa8e4f435d4108caf4cd485581f
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-08-19 21:46:26 +0530

    ➕ add deps

--- lambdas/layers/pack-lambda-layer.sh
+++ lambdas/layers/pack-lambda-layer.sh
@@ -16,6 +16,7 @@ if [ ! -f package.json ]; then
     echo -e "$ERROR_TEXT package.json file not found! Exiting..."
     exit 1
 fi
+echo -e "\e[1;33mPacking Lambda Layer '$1'... \e[0m"
 if [ ! -f "$WORKING_DIR/..layers/$1/nodejs" ]; then
     NORMAL_PATH=$(realpath "$WORKING_DIR/../layers")
     echo -e "$INFO_TEXT '$NORMAL_PATH/v1/nodejs' was not found. Creating directory..."
@@ -29,3 +30,13 @@ echo -e "$INFO_TEXT Zipping '$1' lambda layer..."
 cd "$WORKING_DIR/../layers/$1"
 zip -r "$WORKING_DIR/../layers/$1.zip" "nodejs"
 echo -e "$INFO_TEXT Lambda layer '$1' ready to be uploaded!"
+echo -e "\e[1;33mDo you want to upload the lambda layer now (N/y)? \e[0m"
+read INPUT_CHOICE
+case $INPUT_CHOICE in
+Y | y)
+    cd ../ && chmod +x ./upload-lambda-layer.sh && ./upload-lambda-layer.sh $1
+    ;;
+*)
+    echo -e "$INFO_TEXT \e[1;33mExecute 'cd ../layers && ./upload-lambda-layer.sh $1' for uploading the lambda layer!\e[0m"
+    ;;
+esac
--- lambdas/layers/upload-lambda-layer.sh
+++ lambdas/layers/upload-lambda-layer.sh
@@ -12,7 +12,13 @@ if [ ! -f "$1.zip" ]; then
     echo -e "$ERROR_TEXT $1.zip file not found! Exiting..."
     exit 1
 fi
+echo -e "\e[1;33m Uploading Lambda Layer '$1'... \e[0m"
 aws lambda publish-layer-version --layer-name "githubcommunitysrm-$1" --description "Layer $1 by GitHub Community SRM" \
     --license-info "MIT" --zip-file "fileb://$1.zip" \
     --compatible-runtimes nodejs14.x \
     --profile gcsrm
+if [ $? -eq 0 ]; then
+    echo -e "$INFO_TEXT Lambda Layer '$1' succesfully uploaded!"
+else
+    echo -e "$ERROR_TEXT AWS Upload Error!"
+fi
