0d6694d2d1a2f566f7edc4e706c0e666501a9ea8
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-08-22 04:24:14 +0530

    ✨ add maintainers to internal team

--- lambdas/create-project/src/services/parsers.ts
+++ lambdas/create-project/src/services/parsers.ts
@@ -2,6 +2,11 @@ import { customAlphabet } from "nanoid";
 import { lowercase, numbers } from "nanoid-dictionary";
 import slugify from "slugify";
 
+/**
+ * Function used to generate team unique slug
+ * @param {string} team The team name fetched from the event
+ * @returns {Promise<string>} The unique team-slug
+ */
 export const generateTeamID = async (team: string): Promise<string> => {
   const slug = slugify(team);
   const id_1 = customAlphabet(lowercase, 4)();
@@ -14,11 +19,21 @@ export const generateTeamID = async (team: string): Promise<string> => {
   return `${slug}-${id.join("")}`;
 };
 
+/**
+ * Generates a year-name slug, for example `AY2021-22`
+ * @param {string} year The 4-digit year
+ * @returns {Promise<string>} The year-name slug
+ */
 export const generateYearID = async (year: string): Promise<string> => {
   const suffix = +year.slice(-2);
   return `AY${year}-${suffix + 1}`;
 };
 
+/**
+ * Parses the team-slug generated from the previous steps in the form of `AYXXXX-ZZ/abcd-efgh-1234:::12345/90876`
+ * @param slug
+ * @returns {{"parent-slug": string; "team-slug": string; "org-id": number; "team-id": number;}} The parsed data
+ */
 export const parseTeamSlug = (
   slug: string
 ): {
