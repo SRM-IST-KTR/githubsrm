56594e3b709a85618f3e8d5cf6a0115fe8151789
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-08-21 23:13:09 +0530

    ✨ add child team creation

--- lambdas/create-project/src/services/parsers.ts
+++ lambdas/create-project/src/services/parsers.ts
@@ -4,9 +4,9 @@ import slugify from "slugify";
 
 export const generateTeamID = async (team: string): Promise<string> => {
   const slug = slugify(team);
-  const id_1 = customAlphabet(lowercase, 4);
-  const id_2 = customAlphabet(numbers, 4);
-  return `${slug}-${id_1}-${id_2}`;
+  const id_1 = customAlphabet(lowercase, 4)();
+  const id_2 = customAlphabet(numbers, 4)();
+  return `${slug}-${id_1}${id_2}`;
 };
 
 export const generateYearID = async (year: string): Promise<string> => {
