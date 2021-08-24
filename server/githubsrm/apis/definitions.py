9cadc893324a7e1ee2b46a9a05e2e4123d29580c
Ansh Sharma <60016461+DaemonOnCode@users.noreply.github.com>
2021-08-25 01:26:20 +0530

    ✨ Trigger sns on project porting

--- server/githubsrm/apis/definitions.py
+++ server/githubsrm/apis/definitions.py
@@ -1,9 +1,11 @@
+from requests.api import request
 from schema import Schema, And, SchemaError
 from typing import Any, Callable, Dict
 import re
 import requests
 import cProfile
 
+session = requests.Session()
 
 def get_json_schema(id: int, valid_schema: Callable) -> dict:
     """Generate json schema
@@ -27,7 +29,7 @@ def check_github_id(github_id: str):
     if len(github_id.strip()):
         _url = f"https://github.com/{github_id}"
 
-        response = requests.get(_url)
+        response = session.get(_url)
 
         return response.status_code == 200
     return False
@@ -41,8 +43,8 @@ def check_repo(url: str):
     """
     if len(url) == 0:
         return True
-    
-    response = requests.get(url)
+
+    response = session.get(url)
     return response.status_code == 200
 
 
