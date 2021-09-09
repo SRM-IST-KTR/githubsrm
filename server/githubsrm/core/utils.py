2bb08e9818425ee6a080d3b02dd1617a8483b740
Your Name <as7122000@gmail.com>
2021-09-09 15:31:18 +0530

    🚚 email statics to a json file

--- server/githubsrm/core/utils.py
+++ server/githubsrm/core/utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 from functools import cache
 from typing import Any, Dict
@@ -15,6 +16,11 @@ def get_email_content(role: str, data: Dict[str, str]) -> Dict[str, Any]:
         Dict[str, Any]: [description]
     """
 
+    templates_folder = find_templates_folder()
+
+    with open(f"{templates_folder}/email_statics.json", "r") as fp:
+        email_statics = json.load(fp)
+
     if role not in email_statics:
         raise ValueError("role not found, wrong role passed")
 
@@ -26,7 +32,7 @@ def get_email_content(role: str, data: Dict[str, str]) -> Dict[str, Any]:
             f"inconsistent data, data should have the following keys {email_context['kw']}"
         )
 
-    path = f"{find_templates_folder()}/{email_context['file']}.html"
+    path = f"{templates_folder}/{email_context['file']}.html"
 
     html_email = None
     with open(path) as file_:
@@ -43,7 +49,7 @@ def get_email_content(role: str, data: Dict[str, str]) -> Dict[str, Any]:
             "Subject": {"Data": email_context["subject"], "Charset": "utf-8"},
             "Body": {
                 "Text": {
-                    "Data": email_context["bodyText"].format(**body_kw),
+                    "Data": email_context["bodyText"].format(*body_kw),
                     "Charset": "utf-8",
                 },
                 "Html": {"Data": html_email, "Charset": "utf-8"},
@@ -58,122 +64,3 @@ def find_templates_folder():
         if "templates" in dirs:
             return f"{root}/templates"
     raise IOError("templates folder not found")
-
-
-email_statics = {
-    "project_submission_confirmation": {
-        "subject": "Project Idea Submission Confirmation | GitHub Community SRM",
-        "bodyText": "Your project idea {} was received and will be reviewed",
-        "body_kw": ["project_name"],
-        "file": "1",
-        "kw": ["email", "name", "project_name", "project_description"],
-    },
-    "project_submission_approval_w_password_link": {
-        "subject": "Project Idea Submission Approval | GitHub Community SRM",
-        "bodyText": "Your project idea {} was approved",
-        "body_kw": ["project_name"],
-        "file": "2",
-        "kw": ["name", "project_name", "reset_token", "project_id"],
-    },
-    "project_submission_approval": {
-        "subject": "Project Idea Submission Approval | GitHub Community SRM",
-        "bodyText": "Project Idea Submission Approval | GitHub Community SRMProject Idea Submission Approval | GitHub Community SRMYour project idea {} was approved",
-        "body_kw": ["project_name"],
-        "file": "3",
-        "kw": ["name", "email", "project_name", "project_id"],
-    },
-    "maintainer_received": {
-        "subject": "Maintainer Application Received | GitHub Community SRM",
-        "bodyText": "Your application to apply to the project {} has been received",
-        "body_kw": ["project_name"],
-        "file": "4",
-        "kw": ["name", "email", "project_name"],
-    },
-    "contributor_application_to_maintainer": {
-        "subject": "New Contributor Notification | GitHub Community SRM",
-        "bodyText": "New contributor notification to {}",
-        "body_kw": ["name"],
-        "file": "12",
-        "kw": [
-            "name",
-            "email",
-            "project_name",
-            "contributor_name",
-            "contributor_email",
-        ],
-    },
-    "welcome_maintainer_w_password_link": {
-        "subject": "Welcome Maintainer | GitHub Community SRM",
-        "bodyText": "Your application to apply to the project {} has been approved",
-        "body_kw": ["project_name"],
-        "file": "5",
-        "kw": ["name", "email", "project_name", "reset_token"],
-    },
-    "welcome_maintainer": {
-        "subject": "Welcome Maintainer | GitHub Community SRM",
-        "bodyText": "Your application to apply to the project {} has been received",
-        "body_kw": ["project_name"],
-        "file": "6",
-        "kw": [
-            "name",
-            "email",
-            "project_name",
-        ],
-    },
-    "new_maintainer_notification": {
-        "subject": "New Maintainer Notification | GitHub Community SRM",
-        "bodyText": "A new maintainer has applied to your project {}",
-        "body_kw": ["project_name"],
-        "file": "7",
-        "kw": ["name", "email", "project_name", "beta_name", "beta_email"],
-    },
-    "project_approval": {
-        "subject": "Project Approval | GitHub Community SRM",
-        "bodyText": "Your project {} has been approved",
-        "body_kw": ["project_name"],
-        "file": "8",
-        "kw": ["name", "email", "project_name", "project_url", "project_id"],
-    },
-    "contributor_received": {
-        "subject": "Contributor Application Received | GitHub Community SRM",
-        "bodyText": "Your application to be a contributor to the project {} has been received",
-        "body_kw": ["project_name"],
-        "file": "9",
-        "kw": ["name", "email", "project_name", "contribution"],
-    },
-    "contributor_approval": {
-        "subject": "Contributor Application Approval | GitHub Community SRM",
-        "bodyText": "Your application to be a contributor to the project {} has been approved",
-        "body_kw": ["project_name"],
-        "file": "10",
-        "kw": ["name", "email", "project_name", "project_url"],
-    },
-    "forgot_password": {
-        "subject": "Forgot Password | GitHub Community SRM",
-        "bodyText": "Token to reset your password",
-        "body_kw": [],
-        "file": "11",
-        "kw": ["name", "email", "reset_token"],
-    },
-    "maintainer_application_rejection": {
-        "subject": "Maintainer Application Rejection | GitHub Community SRM",
-        "bodyText": "Maintainer Application Rejection",
-        "body_kw": [],
-        "file": "13",
-        "kw": ["name", "email", "project_name"],
-    },
-    "admin_contributor_rejection": {
-        "subject": "Contributor Application Rejection | GitHub Community SRM",
-        "bodyText": "Sent to reject contriutor by admin",
-        "body_kw": [],
-        "file": "14",
-        "kw": ["name", "email", "project_name"],
-    },
-    "maitainer_contributor_rejection": {
-        "subject": "Contributor Application Rejection | GitHub Community SRM",
-        "bodyText": "Sent to reject contributor by maintainer",
-        "body_kw": [],
-        "file": "15",
-        "kw": ["name", "email", "project_name"],
-    },
-}
