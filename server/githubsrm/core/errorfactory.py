75b32b0191cfd7f2cde727ccf60e2a1d5714724b
Aradhya <aradhyatripathi51@gmail.com>
2021-12-14 14:39:00 +0530

    refactor(errors): added default error message

--- server/githubsrm/core/errorfactory.py
+++ server/githubsrm/core/errorfactory.py
@@ -1,53 +1,67 @@
 from rest_framework.exceptions import APIException
 
 
+def set_default_error(detail):
+    if not detail:
+        return dict(error="Error Occurred!")
+    return detail
+
+
 class ContributorErrors(APIException):
-    def __init__(self, detail="Error Occoured!", code=400):
-        super().__init__(detail=detail, code=code)
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
+        self.status_code = 400
+        super().__init__(detail=detail)
 
 
 class MaintainerErrors(APIException):
-    def __init__(self, detail="Error Occoured!", code=400):
-        super().__init__(detail=detail, code=code)
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
+        self.status_code = 400
+        super().__init__(detail=detail)
 
 
 class MiscErrors(APIException):
-    def __init__(self, detail="Error Occoured!", status_code=400):
-        self.status_code = status_code
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
+        self.status_code = 400
         super().__init__(detail=detail)
 
 
 class ProjectErrors(APIException):
-    def __init__(self, detail="Error Occoured!", code=400):
-        super().__init__(detail=detail, code=code)
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
+        self.status_code = 400
+        super().__init__(detail=detail)
 
 
 class AuthenticationErrors(APIException):
-    def __init__(self, detail="Error Occoured!", status_code=401):
-        self.status_code = status_code
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
+        self.status_code = 401
         super().__init__(detail=detail)
 
 
 class AdminErrors(APIException):
-    def __init__(self, detail="Error Occoured!", status_code=400):
-        self.status_code = status_code
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
+        self.status_code = 400
         super().__init__(detail=detail)
 
 
 class ContributorApprovedError(ContributorErrors):
-    def __init__(self, detail="Error Occoured!", status_code=400):
-        self.status_code = status_code
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
         super().__init__(detail=detail)
 
 
 class ContributorNotFoundError(ContributorErrors):
-    def __init__(self, detail="Error Occoured!", status_code=400):
-        self.status_code = status_code
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
         super().__init__(detail=detail)
 
 
-
 class MaintainerNotFoundError(MaintainerErrors):
-    def __init__(self, detail="Error Occoured!", status_code=400):
-        self.status_code = status_code
+    def __init__(self, detail=None):
+        detail = set_default_error(detail)
         super().__init__(detail=detail)
