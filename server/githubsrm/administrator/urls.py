db93b8b12667415565da15864c966d23138dc99e
Aradhya <aradhyatripathi51@gmail.com>
2022-02-24 13:19:15 +0530

    refactor: moved to functional views

--- server/githubsrm/administrator/urls.py
+++ server/githubsrm/administrator/urls.py
@@ -1,9 +1,10 @@
-from .views import ProjectsAdmin, RegisterAdmin, AdminLogin, admin_accepted
 from django.urls import path
 
+from .views import ProjectsAdmin, admin_accepted, admin_login, register_admin
+
 urlpatterns = [
-    path("register", RegisterAdmin.as_view()),
+    path("register", register_admin),
     path("projects", ProjectsAdmin.as_view()),
     path("projects/accepted", admin_accepted),
-    path("login", AdminLogin.as_view()),
+    path("login", admin_login),
 ]
--- server/githubsrm/apis/urls.py
+++ server/githubsrm/apis/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .open_views import ContactUs, Contributor, Maintainer, team
+from .open_views import Contributor, Maintainer, contact_us, team
 
 urlpatterns = [
     path("contributor", Contributor.as_view()),
     path("maintainer", Maintainer.as_view()),
-    path("contact-us", ContactUs.as_view()),
+    path("contact-us", contact_us),
     path("team", team),
 ]
